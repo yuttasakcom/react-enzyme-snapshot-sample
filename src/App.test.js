549ef161c62a8c396880f58370c58eebffd8732a
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-09 16:06:51 +0700

    react-enzyme-snapshot-sample

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+test("expect to render App component", () => {
+  // expect(shallow(<App />)).toMatchSnapshot();
+  const wrapper = shallow(<App />);
+  const appTitle = wrapper.find('[data-test="app-title"]');
+  expect(appTitle.length).toBe(1);
+  expect(appTitle.text()).toMatchSnapshot();
 });
