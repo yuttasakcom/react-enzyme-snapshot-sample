0588f86cfb8dcfda3583fa9f7271ecaff7afe743
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-09 17:59:19 +0700

    add test App Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
 
-const App = () => (
-  <div>
-    <h1 data-test="app-title">App Title Updated</h1>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. At illum
-      temporibus aut magni exercitationem similique dignissimos numquam iste
-      voluptate accusantium laborum unde nisi atque ad nostrum, expedita quis
-      quidem asperiores.
-    </p>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1 data-test="app-title">Counter App</h1>
+        <p data-test="display-counter">{this.state.count}</p>
+        <div>
+          <button
+            data-test="btn-decrement"
+            onClick={() => {
+              if (this.state.count < 1) return;
+              this.setState({ count: this.state.count - 1 });
+            }}
+          >
+            -
+          </button>
+          &nbsp;
+          <button
+            data-test="btn-increment"
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,89 @@ import App from "./App";
 
 configure({ adapter: new Adapter() });
 
-test("expect to render App component", () => {
-  // expect(shallow(<App />)).toMatchSnapshot();
-  const wrapper = shallow(<App />);
-  const appTitle = wrapper.find('[data-test="app-title"]');
-  expect(appTitle.length).toBe(1);
-  expect(appTitle.text()).toMatchSnapshot();
+describe("test counter app", () => {
+  const setup = (props = {}, state = null) => {
+    const wrapper = shallow(<App {...props} />);
+    if (state) {
+      wrapper.setState(state);
+    }
+    return wrapper;
+  };
+
+  test("expect to render App component", () => {
+    const wrapper = setup();
+    expect(wrapper.length).toBe(1);
+  });
+
+  test("snapshot app title", () => {
+    const wrapper = setup();
+    const appTitle = wrapper.find('[data-test="app-title"]');
+    expect(appTitle.length).toBe(1);
+    expect(appTitle.text()).toMatchSnapshot();
+  });
+
+  test("renders display counter", () => {
+    const wrapper = setup();
+    const button = wrapper.find('[data-test="display-counter"]');
+    expect(button.length).toBe(1);
+  });
+
+  test("renders increment button", () => {
+    const wrapper = setup();
+    const button = wrapper.find('[data-test="btn-increment"]');
+    expect(button.length).toBe(1);
+  });
+
+  test("renders decrement button", () => {
+    const wrapper = setup();
+    const button = wrapper.find('[data-test="btn-decrement"]');
+    expect(button.length).toBe(1);
+  });
+
+  test("counter start at 0", () => {
+    const wrapper = setup();
+    expect(wrapper.state("count")).toBe(0);
+  });
+
+  test("clicking button increments counter", () => {
+    const count = 7;
+    const wrapper = setup(null, { count });
+
+    // find button and click
+    const button = wrapper.find('[data-test="btn-increment"]');
+    button.simulate("click");
+    wrapper.update();
+
+    // find display and test value
+    const counterDisplay = wrapper.find('[data-test="display-counter"]');
+    expect(counterDisplay.text()).toEqual("8");
+  });
+
+  test("clicking button decrements counter", () => {
+    const count = 7;
+    const wrapper = setup(null, { count });
+
+    // find button and click
+    const button = wrapper.find('[data-test="btn-decrement"]');
+    button.simulate("click");
+    wrapper.update();
+
+    // find display and test value
+    const counterDisplay = wrapper.find('[data-test="display-counter"]');
+    expect(counterDisplay.text()).toEqual("6");
+  });
+
+  test("clicking button decrements = 0", () => {
+    const count = 0;
+    const wrapper = setup(null, { count });
+
+    // find button and click
+    const button = wrapper.find('[data-test="btn-decrement"]');
+    button.simulate("click");
+    wrapper.update();
+
+    // find display and test value
+    const counterDisplay = wrapper.find('[data-test="display-counter"]');
+    expect(counterDisplay.text()).toEqual("0");
+  });
 });
